b7c8b5377263d03e967c5c2eccad2c5afd7b8917
Maxence (server) <maxencebp92@gmail.com>
2026-05-22 15:35:53 +0200

    learning Results

--- Software/OT_NN/BE_Unet/main.py
+++ Software/OT_NN/BE_Unet/main.py
@@ -15,7 +15,7 @@ from evaluate import evaluate, visualize, visualize_error
 
 
 NETWORK   = 'BE_Unet'  # 'U-net' ou 'BE_Unet'
-user      = 'laptop'   # 'laptop' ou 'server'
+user      = 'server'   # 'laptop' ou 'server'
 name_file = 'dataset_macro'
 
 if user == 'laptop':
@@ -44,7 +44,7 @@ LR          = 1e-3
 EPS_SMAPE   = 1e-6
 
 RESUME = False
-EPOCHS = 1
+EPOCHS = 300
 
 # ═══════════════════════════════════════════════════════════════════════════════
 #%%  Device
